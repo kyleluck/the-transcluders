5a1b23731ec59932c0926537505a9795407ba181
Regan <regs.co@gmail.com>
2016-06-13 12:02:51 -0400

    added factory, controller

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+var APIKEY = "43bcbd9c821513cf95efd29956339792155c7ed3"; //regan
+// var APIKEY = "3ddc099bbb19af0cd00f41ee78920c7b2bb90f7d"; //kyle1
+// var APIKEY = "a2a76189999ce286d4a26875b8ec1d37eec6fc4e"; //kyle2
+// var APIKEY = "88d4c0a96e942887a933223c884fb6281dcebafc"; //anthony
 
+var app = angular.module('app', []);
+
+app.controller('MainController', function($scope, Alchemy) {
+  $scope.search = function() {
+    Alchemy.getData(function(response) {
+      console.log(response);
+    });
+  }; 
+});
+
+app.factory('Alchemy', function($http) {
+  return {
+    getData: function(callback) {
+      $http({
+        url: "https://gateway-a.watsonplatform.net/calls/data/GetNews",
+        params: {
+          apikey: APIKEY,
+          outputMode: 'json',
+          start: "now-3h",
+          end: "now",
+          count: 5,
+          return: "enriched"
+        }
+      }).then(callback);
+    }
+  };
+});

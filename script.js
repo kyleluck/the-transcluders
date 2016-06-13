e45c2c5195c058b71b5f66142b16abf09c74789b
Regan <regs.co@gmail.com>
2016-06-13 13:58:29 -0400

    added search by topic and array of cities.

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-var APIKEY = "43bcbd9c821513cf95efd29956339792155c7ed3"; //regan
+// var APIKEY = "43bcbd9c821513cf95efd29956339792155c7ed3"; //regan
 // var APIKEY = "3ddc099bbb19af0cd00f41ee78920c7b2bb90f7d"; //kyle1
 // var APIKEY = "a2a76189999ce286d4a26875b8ec1d37eec6fc4e"; //kyle2
-// var APIKEY = "88d4c0a96e942887a933223c884fb6281dcebafc"; //anthony
+var APIKEY = "88d4c0a96e942887a933223c884fb6281dcebafc"; //anthony
+
+var cities = [
+  {name: "New York"},
+  {name: "San Francisco"},
+  {name: "Chicago"},
+  {name: "Houston"},
+  {name: "Atlanta"}
+];
 
 var app = angular.module('app', []);
 
 app.controller('MainController', function($scope, Alchemy) {
   $scope.search = function(searchQuery) {
-    Alchemy.getData(searchQuery, function(response) {
-      $scope.resultSet = response.data.result.docs;
-      console.log(response);
-    }, function(response) {
-      alert('API Error. Check Console!');
-      console.log('API Error was: ', response);
+    cities.forEach(function(city) {
+      Alchemy.getData(city, searchQuery, function(response) {
+        $scope.resultSet = response.data.result.docs;
+        console.log(response);
+      }, function(response) {
+        alert('API Error. Check Console!');
+        console.log('API Error was: ', response);
+      });
     });
   };
 });
 
 app.factory('Alchemy', function($http) {
   return {
-    getData: function(searchQuery, callback, errorCallback) {
+    getData: function(city, searchQuery, callback, errorCallback) {
       $http({
         url: "https://gateway-a.watsonplatform.net/calls/data/GetNews",
         params: {
@@ -29,7 +39,8 @@ app.factory('Alchemy', function($http) {
           end: "now",
           count: 5,
           return: "enriched",
-          'q.enriched.url.title': searchQuery
+          'q.enriched.url.text': "A[" + city + "^" + searchQuery+ "]"
+
         }
       }).then(callback, errorCallback);
     }
